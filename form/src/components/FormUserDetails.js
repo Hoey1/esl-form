0aa8acb984f35bbecdecc436cdf6410d58544463
Joey <joey@xm8.ninja>
2020-05-25 19:29:40 -0400

    updated Material UI styling for first test secion

--- form/src/components/FormUserDetails.js
+++ form/src/components/FormUserDetails.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
+// import SchoolIcon from "@material-ui/icons/School";
 
 export class FormUserDetails extends Component {
   continue = (e) => {
@@ -16,10 +17,47 @@ export class FormUserDetails extends Component {
       <MuiThemeProvider>
         <React.Fragment>
           <AppBar title="Enter User Details" />
+          {/* <SchoolIcon color="primary" /> */}
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+            // onChange={handleChange("firstName")}
+            onChange={this.props.handleChange}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Last Name"
+            floatingLabelText="Last Name"
+            // onChange={handleChange("lastName")}
+            onChange={this.props.handleChange}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Email"
+            floatingLabelText="Email"
+            // onChange={handleChange("email")}
+            onChange={this.props.handleChange}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            styles={styles.button}
+            onClick={this.continue}
+          />
         </React.Fragment>
       </MuiThemeProvider>
     );
   }
 }
+
+const styles = {
+  button: {
+    margin: 15,
+  },
+};
 
 export default FormUserDetails;
